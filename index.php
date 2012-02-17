978e21c5a32264d6d268027aea3b8f8f86263daf
Sebastian Buckpesch <s.buckpesch@iconsultants.eu>
2012-02-17 16:46:43 +0100

    Update base css style. Use bootstrap

--- index.php
+++ index.php
@@ -24,79 +24,149 @@ require_once('init.php');
 
   <!-- Place favicon.ico and apple-touch-icon.png in the root directory: mathiasbynens.be/notes/touch-icons -->
 
-  <link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<style>
+	body {
+	  padding-top: 60px;
+	  padding-bottom: 40px;
+	}
+	</style>
+	<link rel="stylesheet" href="css/bootstrap-responsive.css">
+	<link rel="stylesheet" href="css/style.css">
 
-  <!-- More ideas for your <head> here: h5bp.com/d/head-Tips -->
-
-  <!-- All JavaScript at the bottom, except this Modernizr build.
-       Modernizr enables HTML5 elements & feature detects for optimal performance.
-       Create your own custom Modernizr build: www.modernizr.com/download/ -->
-  <script src="js/libs/modernizr-2.0.6.min.js"></script>
+	<script src="js/libs/modernizr-2.5.2-respond-1.1.0.min.js"></script>
 </head>
 <body>
-  <!-- Prompt IE 6 users to install Chrome Frame. Remove this if you support IE 6.
+	<!-- Prompt IE 6 users to install Chrome Frame. Remove this if you support IE 6.
        chromium.org/developers/how-tos/chrome-frame-getting-started -->
-  <!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
-  <header>
-
-  </header>
-  <div role="main">
-
-  </div>
-  <footer>
-		<?$aa->getConfig("config_value_id");?>
-  </footer>
-
-
-  <!-- JavaScript at the bottom for fast page loading -->
-
-  <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-  <script>window.jQuery || document.write('<script src="js/libs/jquery-1.7.1.min.js"><\/script>')</script>
-
-  <!-- scripts concatenated and minified via build script -->
-  <script src="js/plugins.js"></script>
-  <script src="js/script.js"></script>
-  <!-- end scripts -->
-
-  <!-- Asynchronous Google Analytics snippet. Change UA-XXXXX-X to be your site's ID.
-       mathiasbynens.be/notes/async-analytics-snippet -->
-  <script>
-    var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
-    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-    s.parentNode.insertBefore(g,s)}(document,'script'));
-  </script>
-  
-  <div id="fb-root"></div>
-
-<script type="text/javascript">
-	jQuery(document).ready(function($) {
-
-		initApp();
-
-	});
+	<!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
 	
-	window.fbAsyncInit = function() {
-	    FB.init({
-	      appId      : 'YOUR_APP_ID', // App ID
-	      channelUrl : '//WWW.YOUR_DOMAIN.COM/channel.html', // Channel File
-	      status     : true, // check login status
-	      cookie     : true, // enable cookies to allow the server to access the session
-	      xfbml      : true  // parse XFBML
-	    });
+	<div id="header-container">
+		<header class="wrapper clearfix">
+			<div class="navbar navbar-fixed-top">
+	      		<div class="navbar-inner">
+			        <div class="container">
+			          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+			            <span class="icon-bar"></span>
+			            <span class="icon-bar"></span>
+			            <span class="icon-bar"></span>
+			          </a>
+			          <div class="nav-collapse collapse" style="height: 0px; ">
+			          	<nav>
+				            <ul class="nav">
+				              <li class="active"><a href="#">Homepage</a></li>
+				              <li><a href="#item2">Menu-item2</a></li> 
+				              <li><a href="#item3">Menu-item3</a></li>
+				            </ul>
+			            </nav>
+			          </div><!--/.nav-collapse -->
+			        </div>
+			    </div>
+	    	</div>
+	    	<a id="logo" class="brand" href="#"><img src="#" alt="Logo" /></a>
+	    </header>
+	</div>
 
-	    // Additional initialization code here
-	  };
+	<div id="main-container" class="container">
+		<div id="main" class="wrapper clearfix">
+			<!-- Main hero unit for a primary marketing message or call to action -->
+			<div class="hero-unit">
+				<h1>Hello, world!</h1>
+				<p>This is a template for a simple marketing or informational
+					website. It includes a large callout called the hero unit and three
+					supporting pieces of content. Use it as a starting point to create
+					something more unique.</p>
+				<p>
+					<a class="btn btn-primary btn-large">Learn more &raquo;</a>
+				</p>
+			</div>
 
-	  // Load the SDK Asynchronously
-	  (function(d){
-	     var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
-	     js = d.createElement('script'); js.id = id; js.async = true;
-	     js.src = "//connect.facebook.net/en_US/all.js";
-	     d.getElementsByTagName('head')[0].appendChild(js);
-	   }(document));
-</script>
+			<!-- Example row of columns -->
+			<div class="row">
+				<div class="span4">
+					<h2>Heading</h2>
+					<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus,
+						tellus ac cursus commodo, tortor mauris condimentum nibh, ut
+						fermentum massa justo sit amet risus. Etiam porta sem malesuada
+						magna mollis euismod. Donec sed odio dui.</p>
+					<p>
+						<a class="btn" href="#">View details &raquo;</a>
+					</p>
+				</div>
+				<div class="span4">
+					<h2>Heading</h2>
+					<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus,
+						tellus ac cursus commodo, tortor mauris condimentum nibh, ut
+						fermentum massa justo sit amet risus. Etiam porta sem malesuada
+						magna mollis euismod. Donec sed odio dui.</p>
+					<p>
+						<a class="btn" href="#">View details &raquo;</a>
+					</p>
+				</div>
+				<div class="span4">
+					<h2>Heading</h2>
+					<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in,
+						egestas eget quam. Vestibulum id ligula porta felis euismod
+						semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris
+						condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+					<p>
+						<a class="btn" href="#">View details &raquo;</a>
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<footer>
+		<?$aa->getConfig("config_value_id");?>
+  	</footer>
+
+
+  	<!-- JavaScript at the bottom for fast page loading -->
+
+  	<!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
+  	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+  	<script>window.jQuery || document.write('<script src="js/libs/jquery-1.7.1.min.js"><\/script>')</script>
+
+	<!-- scripts concatenated and minified via build script -->
+  	<script src="js/plugins.js"></script>
+  	<script src="js/script.js"></script>
+  	<!-- end scripts -->
+
+  	<!-- Asynchronous Google Analytics snippet. Change UA-XXXXX-X to be your site's ID.
+       mathiasbynens.be/notes/async-analytics-snippet -->
+	<script>
+	    var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+	    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+	    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+	    s.parentNode.insertBefore(g,s)}(document,'script'));
+  	</script>
+  
+  	<div id="fb-root"></div>
+	<script type="text/javascript">
+		jQuery(document).ready(function($) {
+			initApp();
+		});
+		
+		window.fbAsyncInit = function() {
+		    FB.init({
+		      appId      : '<?=$aa_app_id?>', // App ID
+		      //channelUrl : '//WWW.YOUR_DOMAIN.COM/channel.html', // Channel File
+		      status     : true, // check login status
+		      cookie     : true, // enable cookies to allow the server to access the session
+		      xfbml      : true  // parse XFBML
+		    });
+		    // Additional initialization code here
+		  };
+	
+		  // Load the SDK Asynchronously
+		  (function(d){
+		     var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
+		     js = d.createElement('script'); js.id = id; js.async = true;
+		     js.src = "//connect.facebook.net/en_US/all.js";
+		     d.getElementsByTagName('head')[0].appendChild(js);
+		   }(document));
+	</script>
   
 </body>
 </html>
--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-/* Author: Guntram Pollock
-
-*/
+/** 
+ * Author: Guntram Pollock & Sebastian Buckpesch
+ */
 
 
 
@@ -8,23 +8,16 @@
  * Initializes the content functionality of the app.
  */
 function initApp(){
-	
 	// initialize the menu buttons with onclick functions, which load the according template into the #main-div and save the landing content to display (the first menu-item).
 	var landingContent = initMenu();
 	
 	// set the first menu item as the landing content.
 	$("#main").slideUp( 0, function(){
-		
 		$("#main").load( "templates/" + landingContent + ".html", function(){
-			
 			$("#main").slideDown();
-			
 		});
-		
 	});
-	
 }
-
 
 
 /**
@@ -36,112 +29,59 @@ function initApp(){
  * @return the first menu class name, "default" if there is none.
  */
 function initMenu(){
-	
-	// if no class is found the default class will be loaded and returned at the end.
-	var firstMenuItem = "default";
-	
-	// determine if it was the first item to only save this for returning it later on.
-	var firstTemplate = true;
+	var firstMenuItem = "default"; // if no class is found the default class will be loaded and returned at the end.
+	var firstTemplate = true; // determine if it was the first item to only save this for returning it later on.
 	
 	// loop through all menu elements (a-tags)
 	$('a').each( function( index ) {
-	    	
-//alert("index: " + index + "\nclass: " + $(this).attr("class"));
-		
-		// determine after the loop if a template class was found
-    	var foundTemplate = false;
-    	
-    	// determine if there were classes to load the default template at the end if not
-    	var loadDefault = true;
-		
-		// get all classes from this element
-    	var thisClassAttr = $(this).attr("class");
+    	var foundTemplate = false; // determine after the loop if a template class was found
+    	var loadDefault = true; // determine if there were classes to load the default template at the end if not
+    	var thisClassAttr = $(this).attr("class"); // get all classes from this element
     	
     	// get an array containing each class in one element (only if there is at least one class, otherwise split() will not work and break the script)
     	if( typeof( thisClassAttr ) != "undefined" ) {
-    		
     		thisClasses   = thisClassAttr.split(" ");
-    		
-    		// set the limit for the loop below
-    		classCount = thisClasses.length;
-    		
+    		classCount = thisClasses.length; // set the limit for the loop below
     		loadDefault = false;
-    		
     	} else {
-    		
-    		// do not loop below (this element has no classes)
-    		classCount = 0;
-    		
+    		classCount = 0; // do not loop below (this element has no classes)
     	}
-    	
     	// loop through the classes 
     	for(var x = 0; x < classCount; x++) {
     		
     		// check if one of the classes contain a template class
     		if( thisClasses[x].indexOf("template") >= 0 ) {
-    			
-    			// template class found, get the filename (format: "welcome-filename")
-    			var templateToLoad = thisClasses[x].split("-")[1];
-    			
-//alert("element " + index + " has a template class: " + thisClasses[x] + "\nusing template: " + templateToLoad + ".html" );
-    			
-    			// bind an onclick function to this menu element
-    			$(this).click( function(){
+    			var templateToLoad = thisClasses[x].split("-")[1]; // template class found, get the filename (format: "welcome-filename")
+    			$(this).click( function(){ // bind an onclick function to this menu element
     				
     				// if clicked, load the template into the #main div (append ".html" to the template filename)
     				$("#main").slideUp( 0, function(){
-    					
     					$("#main").load( "templates/" + templateToLoad + ".html", function(){
-    						
     						$("#main").slideDown();
-    						
     					});
-    					
     				});
-    				
     			});
     			
-    			// found a template class, remember that!
     			foundTemplate = true;
-    			
-    			// if it is the first menu-template item, save it to return it later
+
     			if( firstTemplate == true ) {
-    				
 	    			firstMenuItem = templateToLoad;
-	    			
-	    			// remember that this was the first one
 	    			firstTemplate = false;
-	    			
     			}
-    			
     		}
-    		
     	} // end loop through the classes of this element
     	
     	// if no template class was found, use the default one
 		if( foundTemplate == false && loadDefault == true ){
-			
-//alert("element " + index + " has no template class, using default" );
-			
 			$(this).click(function(){
-				
 				$("#main").slideUp( 0, function(){
-					
 					$("#main").load( "templates/default.html", function(){
-						
 						$("#main").slideDown();
-						
 					});
-					
 				});
-				
 			});
-			
 		}
-	    
 	}); // end loop through all menu elements
-	
 	return firstMenuItem;
-	
 };
 
