18c19393a3c75f5afaa4b4e1b76d7194d1849f6e
Sebastian Buckpesch <s.buckpesch@iconsultants.eu>
2012-09-24 19:00:40 +0200

    debuggin

--- modules/newsletter/Newsletter.php
+++ modules/newsletter/Newsletter.php
@@ -31,6 +31,7 @@ class Newsletter {
 			`is_confirmed` tinyint(1) NOT NULL COMMENT 'Is registration confirmed?',
 			PRIMARY KEY (`id`)
 			) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+			var_dump($sql);
 			$res = $this->db->query($sql);
 			var_dump($res);
 				
