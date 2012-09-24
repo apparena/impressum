19aeee54da32c62291ba14026cd53eedff277e48
Sebastian Buckpesch <s.buckpesch@iconsultants.eu>
2012-09-24 18:25:16 +0200

    update db access data

--- config.php
+++ config.php
@@ -9,11 +9,11 @@ $aa_app_secret 	= "770a51440ac4e9c2dbe7345a791fbfc4";
 /**
  * Setup your database access data
  */
-$db_activated 	= false;
+$db_activated 	= true;
 $db_host 		= "localhost";
 $db_name 		= "apps_aa_template";
 $db_user 		= "aa_template";
-$db_pass 		= "abcdefghijklmnopqrstuvwxyz";
+$db_pass 		= "template";
 
 
 ?>
