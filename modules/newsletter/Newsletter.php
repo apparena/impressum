60c49abdc16ab6d12e37cdb3eade65b41ecd32e7
Sebastian Buckpesch <s.buckpesch@iconsultants.eu>
2012-09-25 14:59:50 +0200

    debugging

--- modules/newsletter/Newsletter.php
+++ modules/newsletter/Newsletter.php
@@ -160,7 +160,7 @@ class Newsletter {
 		// Get fb_user_id from email-address
 		$sql = "SELECT email FROM `nl_registration` WHERE `email`='" . $receiver_email . "'
 				AND aa_inst_id=" . $this->aa_inst_id . " LIMIT 1";
-		$receiver_existing = $db->query($sql);
+		$receiver_existing = $this->db->query($sql);
 		
 		if ( $receiver_existing ) {
 			$sql = "UPDATE `nl_registration`
