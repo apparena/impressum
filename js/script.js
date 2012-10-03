9e6747ec5f76c0a4f8bfe1c677d513235ca72dc5
Sebastian Buckpesch <s.buckpesch@iconsultants.eu>
2012-10-03 16:45:57 +0200

    add message function

--- js/script.js
+++ js/script.js
@@ -26,5 +26,48 @@ function aa_tmpl_load(tmpl_filename){
 	}); 
 }
 
+/**
+ * Show a message in the top msg-container (which is hidden usually)
+ * @param msg the message to display
+ * @param type one of: error, success, info - determines the classing of the msg-div-element.
+ * @param delay number of milisecons until the message box disappears
+ */
+function show_msg( msg, type, delay ) {
+	
+	if ( typeof( type ) !== 'string' ) {
+		type = 'error';
+	}
+	
+	if ( typeof( msg ) !== 'string' ) {
+		msg = __e( 'something_went_wrong' );
+	}
+	
+	var classes = 'alert fade in';
+	
+	switch( type ) {
+	
+		case 'error':
+			classes = 'alert alert-error fade in';
+			break;
+			
+		case 'success':
+			classes = 'alert alert-success fade in';
+			break;
+			
+		case 'info':
+			classes = 'alert alert-success fade in';
+			break;
+			
+		default:
+			classes = 'alert alert-block fade in';
+			break;
+	}
+	
+	$( '#msg-container' ).slideUp( 500, function() {
+		$( '#msg-container' ).alert();
+		$( '#msg-container' ).removeClass().addClass( classes ).html( msg ).slideDown( 500 ).delay(delay).fadeOut('slow');
+	});
+	
+}
 
 	
