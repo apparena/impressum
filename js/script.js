1b2cd241621bc7f30937af22484edcf61d9ad41a
Sebastian Buckpesch <s.buckpesch@iconsultants.eu>
2012-10-03 17:23:19 +0200

    update template loading js

--- js/script.js
+++ js/script.js
@@ -20,6 +20,7 @@ function aa_tmpl_load(tmpl_filename){
 				   xfbml      : true, // parse XFBML
 				   oauth    : true
 				});
+				FB.Canvas.scrollTo(0,0);
 				hide_loading(); // hide the loading screen
 			});    						
 		});    					
