4a194175454a0f72a84a66cf3dd3a7be8f8772f7
Valentin Klein <v.klein@iconsultants.eu>
2012-11-05 15:31:43 +0100

    add new admin_panel

--- modules/admin_panel/admin_panel.php
+++ modules/admin_panel/admin_panel.php
@@ -1,24 +1,42 @@
-<script type="text/javascript">
-$(function(){
-    $('.admin_panel').tabSlideOut({
-        tabHandle: '.handle',                     //class of the element that will become your tab
-        pathToTabImage: 'img/admin_right.jpg', //path to the image for the tab //Optionally can be set using css
-        imageHeight: '122px',                     //height of tab image           //Optionally can be set using css
-        imageWidth: '50px',                       //width of tab image            //Optionally can be set using css
-        tabLocation: 'right',                      //side of screen where tab lives, top, right, bottom, or left
-        speed: 500,                               //speed of animation
-        action: 'click',                          //options: 'click' or 'hover', action to trigger animation
-        topPos: '0px',                          //position from the top/ use if tabLocation is left or right
-        leftPos: '20px',                          //position from left/ use if tabLocation is bottom or top
-        fixedPosition: true,                      //options: true makes it stick(fixed position) on scroll
-    });
-	$('.admin_panel').show();
-});
+<!-- Admin Panel-->
+<?
+require_once(dirname(__FILE__).'/../../init.php');
+
+if (is_fb_user_admin()){
+?>
+<script>
+    function admin_panel_open() {
+        $('.adminpanel').slideDown('slow');
+        $('#admin_panel_button').attr('onclick','admin_panel_close()');
+        $('#arrow').removeClass('icon-chevron-down').addClass('icon-chevron-up');
+    }
+    function admin_panel_close() {
+        $('.adminpanel').slideUp('slow');
+        $('#admin_panel_button').attr('onclick','admin_panel_open()');
+        $('#arrow').removeClass('icon-chevron-up').addClass('icon-chevron-down');
+    }
 </script>
 
-<div class="admin_panel" style="z-index:2050;">
-	<a class="handle" href="admin.php">Admin</a>
-	<div class="admin_position">
-	<? require_once dirname(__FILE__).'/admin.php'; ?>
-	</div>
-</div>
+
+
+<header class="adminpanel hide">
+    <div id="adminpanel_header"><h3><? __p('admin_panel') ?></h3></div>
+    <? require_once dirname(__FILE__) . '/admin.php'; ?>
+</header>
+<div id="admin_panel_button" class="clickable" onclick="admin_panel_close()"><i class="icon-cog"></i>&nbsp;<? __p('admin_panel')?>&nbsp;<i id="arrow" class="icon-chevron-up"></i></div>
+<div style="clear:both"></div>
+
+
+<script>
+    var fb_is_admin = '<?=$is_fb_user_admin?>';
+
+    // Show admin panel if user is admin
+    if(fb_is_admin){
+        $('.adminpanel').slideDown('slow').delay(5000).slideUp('slow' ,function() {
+            $('#admin_panel_button').attr('onclick','admin_panel_open()');
+            $('#arrow').removeClass('icon-chevron-up').addClass('icon-chevron-down');
+        });
+    }
+</script>
+
+<? } ?>
