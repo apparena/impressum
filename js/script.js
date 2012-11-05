edd77c5396e27b064bae047c84288309f0fcfa1f
Valentin Klein <v.klein@iconsultants.eu>
2012-11-05 15:42:45 +0100

    add facebook functions

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-/** 
+/**
  * Author: Guntram Pollock & Sebastian Buckpesch
  */
 
@@ -6,25 +6,25 @@
  * Loads a new template file into the div#main container using jquery animations
  * @param tmpl_filename Filename of the template
  */
-function aa_tmpl_load(tmpl_filename){
-	show_loading(); // show the loading screen
-	$("#main").slideUp( 0, function(){
-		$("#main").load( "templates/" + tmpl_filename + "?aa_inst_id=" + aa_inst_id, function(){
-			$("#main").slideDown(600,function(){
-				//reinit facebook
-				FB.init({
-				   appId      : fb_app_id, // App ID
-				   channelUrl : fb_canvas_url + 'channel.html', // Channel File
-				   status     : true, // check login status
-				   cookie     : true, // enable cookies to allow the server to access the session
-				   xfbml      : true, // parse XFBML
-				   oauth    : true
-				});
-				FB.Canvas.scrollTo(0,0);
-				hide_loading(); // hide the loading screen
-			});    						
-		});    					
-	}); 
+function aa_tmpl_load(tmpl_filename, data){
+    show_loading(); // show the loading screen
+    $("#main").slideUp( 0, function(){
+        $("#main").load( "templates/" + tmpl_filename + "?aa_inst_id=" + aa_inst_id + "&" + data, function(){
+            $("#main").slideDown(600,function(){
+                //reinit facebook
+                FB.init({
+                    appId      : fb_app_id, // App ID
+                    channelUrl : fb_canvas_url + 'channel.html', // Channel File
+                    status     : true, // check login status
+                    cookie     : true, // enable cookies to allow the server to access the session
+                    xfbml      : true, // parse XFBML
+                    oauth    : true
+                });
+                FB.Canvas.scrollTo(0,0);
+                hide_loading(); // hide the loading screen
+            });
+        });
+    });
 }
 
 /**
@@ -34,41 +34,77 @@ function aa_tmpl_load(tmpl_filename){
  * @param delay number of milisecons until the message box disappears
  */
 function show_msg( msg, type, delay ) {
-	
-	if ( typeof( type ) !== 'string' ) {
-		type = 'error';
-	}
-	
-	if ( typeof( msg ) !== 'string' ) {
-		msg = __e( 'something_went_wrong' );
-	}
-	
-	var classes = 'alert fade in';
-	
-	switch( type ) {
-	
-		case 'error':
-			classes = 'alert alert-error fade in';
-			break;
-			
-		case 'success':
-			classes = 'alert alert-success fade in';
-			break;
-			
-		case 'info':
-			classes = 'alert alert-success fade in';
-			break;
-			
-		default:
-			classes = 'alert alert-block fade in';
-			break;
-	}
-	
-	$( '#msg-container' ).slideUp( 500, function() {
-		$( '#msg-container' ).alert();
-		$( '#msg-container' ).removeClass().addClass( classes ).html( msg ).slideDown( 500 ).delay(delay).fadeOut('slow');
-	});
-	
+
+    if ( typeof( type ) !== 'string' ) {
+        type = 'error';
+    }
+
+    if ( typeof( msg ) !== 'string' ) {
+        msg = __e( 'something_went_wrong' );
+    }
+
+    var classes = 'alert fade in';
+
+    switch( type ) {
+
+        case 'error':
+            classes = 'alert alert-error fade in';
+            break;
+
+        case 'success':
+            classes = 'alert alert-success fade in';
+            break;
+
+        case 'info':
+            classes = 'alert alert-success fade in';
+            break;
+
+        default:
+            classes = 'alert alert-block fade in';
+            break;
+    }
+
+    $( '#msg-container' ).slideUp( 500, function() {
+        $( '#msg-container' ).alert();
+        $( '#msg-container' ).removeClass().addClass( classes ).html( msg ).slideDown( 500 ).delay(delay).fadeOut('slow');
+    });
+
 }
 
-	
+function postToFeed(link,picture_url,name,caption,desc) {
+
+    // calling the API ...
+    var obj = {
+        method: 'feed',
+        link: link,
+        picture: picture_url,
+        name: name,
+        caption: caption,
+        description: desc
+    };
+
+    function callback(response) {
+        document.getElementById('msg').innerHTML = "Post ID: " + response['post_id'];
+    }
+
+    FB.ui(obj, callback);
+}
+function sendToFriend(link,name) {
+    FB.ui({
+        method: 'send',
+        name: name,
+        link: link
+    });
+}
+function sendRequest(name, desc, data) {
+    // Use FB.ui to send the Request(s)
+    FB.ui({method: 'apprequests',
+        title: name,
+        message: desc,
+        data: data
+    }, callback);
+}
+
+function callback(response) {
+    console.log(response);
+}
