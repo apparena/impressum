629199d139c94f7b552ac60cabe15a7b0a613fe9
Valentin Klein <v.klein@iconsultants.eu>
2012-11-05 17:05:20 +0100

    add admin_panel

--- modules/admin_panel/admin_panel.php
+++ modules/admin_panel/admin_panel.php
@@ -1,18 +1,20 @@
 <!-- Admin Panel-->
 <?
-require_once(dirname(__FILE__).'/../../init.php');
+require_once(dirname(__FILE__) . '/../../init.php');
 
-if (is_fb_user_admin()){
-?>
+if (is_fb_user_admin()) {
+    ?>
 <script>
     function admin_panel_open() {
+        $('.admin_div').css('z-index', '100');
         $('.adminpanel').slideDown('slow');
-        $('#admin_panel_button').attr('onclick','admin_panel_close()');
+        $('#admin_panel_button').attr('onclick', 'admin_panel_close()');
         $('#arrow').removeClass('icon-chevron-down').addClass('icon-chevron-up');
     }
     function admin_panel_close() {
+        $('.admin_div').css('z-index', '0');
         $('.adminpanel').slideUp('slow');
-        $('#admin_panel_button').attr('onclick','admin_panel_open()');
+        $('#admin_panel_button').attr('onclick', 'admin_panel_open()');
         $('#arrow').removeClass('icon-chevron-up').addClass('icon-chevron-down');
     }
 </script>
@@ -24,7 +26,8 @@ if (is_fb_user_admin()){
     <? require_once dirname(__FILE__) . '/admin.php'; ?>
 </header>
 <span class="admin_banner">
-<div id="admin_panel_button" class="clickable" onclick="admin_panel_close()"><i class="icon-cog"></i>&nbsp;<? __p('admin_panel')?>&nbsp;<i id="arrow" class="icon-chevron-up"></i></div>
+<div id="admin_panel_button" class="clickable" onclick="admin_panel_close()"><i
+        class="icon-cog"></i>&nbsp;<? __p('admin_panel')?>&nbsp;<i id="arrow" class="icon-chevron-up"></i></div>
 </span>
 <div style="clear:both"></div>
 
@@ -33,10 +36,12 @@ if (is_fb_user_admin()){
     var fb_is_admin = '<?=$is_fb_user_admin?>';
 
     // Show admin panel if user is admin
-    if(fb_is_admin){
-        $('.adminpanel').slideDown('slow').delay(5000).slideUp('slow' ,function() {
-            $('#admin_panel_button').attr('onclick','admin_panel_open()');
+    if (fb_is_admin) {
+        $('.admin_div').css('z-index', '100');
+        $('.adminpanel').slideDown('slow').delay(5000).slideUp('slow', function () {
+            $('#admin_panel_button').attr('onclick', 'admin_panel_open()');
             $('#arrow').removeClass('icon-chevron-up').addClass('icon-chevron-down');
+            $('.admin_div').css('z-index', '0');
         });
     }
 </script>
