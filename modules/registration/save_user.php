11f75e8c27afbad4d6fc2d90a8a08a6e1d2d1f8d
guntram <guntrampollock@arcor.de>
2012-11-19 13:25:25 +0100

    chk new registration call to save user

--- modules/registration/save_user.php
+++ modules/registration/save_user.php
@@ -8,7 +8,7 @@
  */
 	
     include_once ( '../../init.php' );
-    
+
     $aa_inst_id = 0;
     if ( isset( $_GET[ 'aa_inst_id' ] ) ) {
     	$aa_inst_id = $_GET[ 'aa_inst_id' ];
@@ -19,7 +19,43 @@
     
     $user = false;
     $response = array(); // this response goes back to the success function of the calling javascript at the end
-    
+
+function parse_signed_request($signed_request, $secret) {
+    list($encoded_sig, $payload) = explode('.', $signed_request, 2);
+
+    // decode the data
+    $sig = base64_url_decode($encoded_sig);
+    $data = json_decode(base64_url_decode($payload), true);
+
+    if (strtoupper($data['algorithm']) !== 'HMAC-SHA256') {
+        error_log('Unknown algorithm. Expected HMAC-SHA256');
+        return null;
+    }
+
+    // check sig
+    $expected_sig = hash_hmac('sha256', $payload, $secret, $raw = true);
+    if ($sig !== $expected_sig) {
+        error_log('Bad Signed JSON signature!');
+        return null;
+    }
+
+    return $data;
+}
+
+function base64_url_decode($input) {
+    return base64_decode(strtr($input, '-_', '+/'));
+}
+
+if ($_REQUEST) {
+    echo '<p>signed_request contents:</p>';
+    $response = parse_signed_request( $_REQUEST['signed_request'], $aa['instance']['fb_app_secret'] );
+    echo '<pre>';
+    print_r($response);
+    echo '</pre>';
+} else {
+    echo '$_REQUEST is empty';
+}
+
     if( isset( $_POST[ 'user' ] ) ) {
     	$user = $_POST[ 'user' ];
     } else {
