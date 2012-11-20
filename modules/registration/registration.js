49764facc7bd28272e8e3cc9ad615d45a68f6ae9
guntram <guntrampollock@arcor.de>
2012-11-20 14:47:59 +0100

    clean up ui elements and ui-function usage

--- modules/registration/registration.js
+++ modules/registration/registration.js
@@ -3,9 +3,10 @@
  * Include this module to use it:
  * <script src="modules/registration/registration.js" type="text/javascript" />
  * Use the functions like this:
- * <form method="post" action="javascript:$.register_user();">...
+ * <form method="post" action="javascript:$.register_form();">...
  * or:
  * <button id="fb_connect" onclick="$.register_fb_connect('email,publish_stream');">...
+ * The user data will be stored in a global object $.register_user_data.
  * @author Guntram Pollock 11/2012
  */
 
@@ -13,8 +14,8 @@
  * This is a bootstrap validation var.
  * It defines the required fields and error handling.
  * The rules-keys are ids/names of form elements like inputs.
- * The <select> elements need to use class validation, so they
- * have to be added additionally (the selects have dynamically generated ids/names).
+ * <select> elements might need to use class validation, so they
+ * have to be added additionally (selects with dynamically generated ids/names).
  * Therefore the jquery validation has to be extended by:
  * $.validator.addClassRules({class_name:{required: true}});
  * This must be done before calling validate(...).
@@ -52,24 +53,27 @@ $.register_bootstrap_form = {
     }
 };
 
+/**
+ * Read the inputs from the user and save them in a global user variable ($.register_user_data).
+ * @param {String} id The selector which will be looped to find fields, which it must contain.
+ * @param {Function} callback A callback function to call when the user data has been stored in the $.register_user_data object.
+ */
 $.register_form = function ( id, callback ) {
 	
-	$( '#progress-form' ).show();
-	
-	var selector = $( 'body' ).find( '.form-registration' );
+	var selector = $( 'body' ).find( '.form-registration' ); // default selector if no id-parameter is set
 	
 	if ( typeof( id ) == 'function' ) {
 		callback = id;
 		id = null;
 	} else {
-		if ( typeof( id ) == '{String}' ) {
+		if ( typeof( id ) == 'string' ) {
 			selector = id;
 		}
 	}
 	
-	$.user_data = {}; // create a global user object to save it later
+	$.register_user_data = {}; // create a global user object to save it later to the db
 	
-	// loop through all input elements of the form and 
+	// loop through all input elements of the form
 	selector.find( 'input' ).each( function( index ) {
 		
 		var key = 'default_' + index; // a default key in case no id and no name is set to the input element
@@ -106,7 +110,7 @@ $.register_form = function ( id, callback ) {
 				
 				if ( $(this).attr( 'value' ).length > 0 ) {
 					if ( key.indexOf( 'email' ) >= 0 ) {
-						key = 'key'; // save the email as the key, identifiying the user. use a field containing the word email in the id or name.
+						key = 'key'; // save the email as the key, identifying the user. use a field containing the word email in the id or name.
 					}
 					value = $.trim( $(this).attr( 'value' ) );
 				}
@@ -115,7 +119,7 @@ $.register_form = function ( id, callback ) {
 				
 		} // end switch through this input elements type attribute value
 		
-		$.user_data[ key ] = value; // add key and value to the user object
+		$.register_user_data[ key ] = value; // add key and value to the user object
 		
 	}); // end loop through all form input elements
 	
@@ -134,8 +138,9 @@ $.register_form = function ( id, callback ) {
 			}
 		}
 		
-		$.user_data[ key ] = {};
-		
+		$.register_user_data[ key ] = {};
+
+        // loop through the options of this select element
 		$(this).find( 'option' ).each( function( aIndex ) {
 			
 			// if this option has a certain class, handle it here...
@@ -157,7 +162,7 @@ $.register_form = function ( id, callback ) {
 		            
 		        }
 				
-				if ( multiSelector.length > 0 ) {
+				if ( multiSelector.length > 1 ) {
 					value = mValue.join( ', ' );
 				}
 				
@@ -169,37 +174,36 @@ $.register_form = function ( id, callback ) {
 			
 		}); // end loop through options
 		
-		$.user_data[ key ] = value;
+		$.register_user_data[ key ] = value;
 		
 	}); // end loop through select elements
-	
-	$( '#progress-form' ).hide();
-	
-	if ( $( '#container_log' ).length > 0 ) {
-		$( '#container_log' ).fadeIn( 300 );
-	}
-	
-	$( '#saveUserData' ).remove();
-	
-	$( '#progress-form' ).after(
-		'<button class="btn btn-success" id="saveUserData" onclick="$.save_user_data();"><i class="icon-download-alt icon-white"></i> Save user data</button>'
-	);
-	
-};
 
-$.register_fb_connect = function ( scope, callback ) {
+    if ( typeof callback == 'function' ) {
+        callback();
+    }
+	
+}; // end register_form()
+
+
+/**
+ * Connects the user via Facebook.
+ * Will use 'email' as a scope if no scope is set.
+ * @param {String} scope The FB.api-scope (permission(s)) can be comma separated like "email, publish_stream".
+ * @param {Function} callbackSuccess This callback gets executed when the user accepts the FB dialog.
+ * @param {Function} callbackError This callback gets executed when the user cancels the FB dialog.
+ */
+$.register_fb_connect = function ( scope, callbackSuccess, callbackError ) {
 	
 	if ( typeof( scope ) == 'function' ) {
-		callback = scope;
-		if ( typeof( $( '#scope' ).val() ) == '{String}' && $( '#scope' ).val().length > 0 ) {
-			scope = $( '#scope' ).val();
-		} else {
-			scope = 'email';
-		}
+        if ( typeof callbackSuccess == 'function' ) {
+            callbackError = callbackSuccess;
+        }
+		callbackSuccess = scope;
+        scope = null;
 	}
-	
-	disableForm();
-	$( '#progress-connect' ).show();
+    if ( typeof scope != 'string' ) {
+        scope = 'email'; // default scope
+    }
 	
 	FB.login( function( response ) {
 		
@@ -209,7 +213,7 @@ $.register_fb_connect = function ( scope, callback ) {
 				
 				if ( typeof( response.id ) != 'undefined' ) {
 					
-					$.user_data = {};
+					$.register_user_data = {};
 					
 					// the response length may vary due to the scope used for the FB.login() function! 
 					for( var key in response ) { // get all values from the response and save them in the user object
@@ -217,175 +221,147 @@ $.register_fb_connect = function ( scope, callback ) {
 						var item = response[ key ];
 						
 						if ( key == 'id' ) {
-							$.user_data[ 'key' ] = item; // use the key 'key' for a fb-user-id as an user-identificator
+							$.register_user_data[ 'key' ] = item; // use the key 'key' for a fb-user-id as an user-identificator
 						} else {
-							$.user_data[ key ] = item;
+							$.register_user_data[ key ] = item;
 						}
 						
 					}
 					
-					if ( $( '#user_profile' ).length > 0 ) {
-						$( '#user_profile' ).html(
-							$.user_data[ 'name' ] + '&nbsp;&nbsp;&nbsp;'
-							+ '<a href="https://www.facebook.com/' + $.user_data[ 'key' ] + '" target="_blank">'
-							+'<img class="profile-picture" src="https://graph.facebook.com/'
-							+ $.user_data[ 'key' ]
-							+ '/picture?type=square" title="visit the users facebook profile..." /></a>'
-						);
-					}
-					
-					if ( $( '#container_log' ).length > 0 ) {
-						$( '#container_log' ).fadeIn( 300 );
-					}
-					
 /*
 					// if you only use 'email' as a scope for FB.login()
-					$.user_data[ 'key' ]   = response.id;
-					$.user_data[ 'name' ]  = response.name;
-					$.user_data[ 'email' ] = response.email;
+					$.register_user_data[ 'key' ]   = response.id;
+					$.register_user_data[ 'name' ]  = response.name;
+					$.register_user_data[ 'email' ] = response.email;
 */
 					
 				}
 				
-				enableForm();
-				$( '#progress-connect' ).hide();
-				
-				$( '#saveUserData' ).remove();
-				$( '#progress-connect' ).after(
-					'<button class="btn btn-success" id="saveUserData" onclick="$.save_user_data();"><i class="icon-download-alt icon-white"></i> Save user data</button>'
-				);
-				
-				if ( typeof( callback ) == 'function' ) {
-					callback();
+				if ( typeof( callbackSuccess ) == 'function' ) {
+                    callbackSuccess();
 				}
 				
-			});
+			}); // end FB.api call
 			
 		} else {
 			// the user did not accept the FB.login() authorization request!
-			enableForm();
-			$( '#progress-connect' ).hide();
-			
-			aa_tmpl_load( 'no_auth.phtml' );
+            if ( typeof( callbackError ) == 'function' ) {
+                callbackError();
+            }
 		}
 		
-    }, { scope: scope });
+    }, { scope: scope }); // end FB.login call
 	
-};
+}; // end register_fb_connect()
 
 
 /**
- * Shows the fb-register form in the desired id.
+ * Shows the fb-register form widget in the desired id.
  * If the params are empty, it will try to display the form appended to the body.
- * @param fields The fields to query from the user. These fields will be available later to save them in the db.
- * @param url The url will be called by Facebook including a signed request when the user confirms the form.
- * @param put_to_id Specify a HTML-id to put the registration form into after creating it.
+ * Note that the fb widget might show up an error message, e.g. if 'name' is not the first field or the fields are not set up correctly.
+ * You can specify own input elements by passing a JSON object rather than a comma-separated string.
+ * Note that if you do not pass a valid string or json with the fields, the default field 'name' will be used.
+ * Note that the put_to parameter will be defaulted to 'body' if it is missing, so that the widget will be appended to the body by default.
+ * @param {String|Object} fields The fields to query from the user. These fields will be available later to save them in the db.
+ * @param {String} url The url will be called by Facebook including a signed request when the user confirms the form.
+ * @param {String} put_to Specify an HTML-element by a selector to put the registration form into after creating it, e.g. '#myId', '.myClass'.
+ * @return {Object} This function will return an error object if the put_to element selector is missing, or a success object if there was no error.
  */
-$.register_fb_widget = function ( fields, url, put_to_id ) {
+$.register_fb_widget = function ( fields, url, put_to ) {
 
     if ( typeof( fields ) == 'undefined' || fields.length <= 0 ) {
-        if ( $( '#fields' ).length > 0 ) {
-            fields = $( '#fields' ).val();
-        } else {
-            fields = 'name'; // at least one default field
-        }
+        fields = 'name'; // at least one default field
     }
 
     if ( typeof( url ) == 'undefined' || url.length <= 0 ) {
-        if ( $( '#url' ).length > 0 ) {
-            url = $( '#url' ).val();
-        } else {
-            // default url
-            url = 'https://www.app-arena.com/app/aa_template/dev/modules/registration/save_user.php?aa_inst_id=' + aa_inst_id;
-        }
+        // default url
+        url = 'https://www.app-arena.com/app/aa_template/dev/modules/registration/save_user.php?aa_inst_id=' + aa_inst_id;
     }
 
-    if ( typeof( put_to_id ) == "undefined" || $( '#' + put_to_id ).length <= 0 ) {
-        put_to_id = 'fb_registration';
+    if ( typeof( put_to ) == "undefined" || $( '' + put_to ).length <= 0 ) {
+        put_to = 'body';
     }
 
-    if ( $( '#' + put_to_id ).length <= 0 ) {
-        $( 'body').append( '<div id="fb_registration"></div>' );
+    if ( $( '' + put_to ).length <= 0 ) {
+        return {"error":"you must provide an element where the generated fb-widget will be placed"};
     }
 
-    var fb_registration = '<div class="row span8"></div><fb:registration '
+    if ( $( '#register_fb_widget').length <= 0 ) {
+        $( '#register_fb_widget' ).remove(); // remove the div if it is already there to prevent duplicate widgets showing up
+    }
+
+    var fb_registration = '<div id="register_fb_widget"><fb:registration '
         + 'fields="' + fields + '" '
         + 'redirect-uri="' + url + '" '
         + 'width="530" '
-        /*+ 'on-login="log_login(arguments);"'*/
-        + ' ></fb:registration>';
+        + ' ></fb:registration></div>';
 
-    $( '#' + put_to_id ).html( fb_registration );
+    $( '' + put_to ).append( fb_registration );
 
-    /*
-     * the login button will not send post or request data to the url!
-     *
-     fb_registration = '<div class="row span8">or by login button:</div>'
-     + '<div class="row span8"><fb:login-button '
-     + 'registration-url="' + url + '" '
-     + 'on-login="log_login(arguments);" '
-     + ' /></div>';
+    FB.XFBML.parse(); // render the xfbml tag <fb:registration>
 
-     $( '#' + put_to_id ).append( fb_registration );
-     */
+    return {"success":true};
 
-    FB.XFBML.parse();
+}; // end register_fb_widget()
 
-};
-
-
-$.register_save_user_data = function() {
-	
-	disableForm();
-	$( '#progress-form' ).show();
+/**
+ * Save the user data objects content to the db.
+ * Calls the save_user.php file which will do the save process.
+ * @param {Function} callbackSuccess <p>This function will be called when the save_user.php has finished saving the user / determining if the user existed.
+ *                                   The received response will be passed to the callback function as a parameter.</p>
+ * @param {Function} callbackError This function will be called when the save_user.php file is not found or the server is down.
+ */
+$.register_save_user_data = function( callbackSuccess, callbackError ) {
 	
 	$.ajax({
 		type : 'POST',
 		async : true,
 		url : 'modules/registration/save_user.php?aa_inst_id=' + aa_inst_id,
 		data : ({
-			user: $.user_data
+			user: $.register_user_data
 		}),
-		success : function(data) {
-			
-			$( '#progress-form' ).hide();
-			enableForm();
-			
-		}
-	});
+		success : function( data ) {
+            if ( typeof callbackSuccess == 'function' ) {
+                callbackSuccess( data );
+            }
+		},
+        error: function( jqXHR, textStatus, errorThrown ) {
+            if ( typeof callbackError == 'function' ) {
+                var errorObject = {};
+                errorObject[ 'jqXHR' ]       = jqXHR;
+                errorObject[ 'textStatus' ]  = textStatus;
+                errorObject[ 'errorThrown' ] = errorThrown;
+                callbackError( errorObject );
+            }
+        }
+	}); // end save_user.php ajax call
 	
-};
+}; // end register_save_user_data()
 
 /**
  * Log an action for an existing user.
  * @param {String} action the action type to log, e.g. 'register' or 'invite'.
  * @param {String} data the additional data to save for this log item, e.g. FB user ids of invited friends.
+ * @param {Function} callbackSuccess <p>This function will be called when the log_user_action.php has finished saving the log.
+ *                                   The received response will be passed to the callback function as a parameter.</p>
+ * @param {Function} callbackError This function will be called when the log_user_action.php file is not found or the server is down.
  */
-$.register_log_action = function ( action, data ) {
-	
-	disableForm();
-	$( '#progress-log' ).show();
+$.register_log_action = function ( action, data, callbackSuccess, callbackError ) {
 	
 	$.user_log = {};
 	
 	$.user_log[ 'action' ] = 'register';
-	$.user_log[ 'data' ]   = 'sample data';
-	
-	if ( $( '#action' ).length > 0 ) {
-		if ( $( '#action' ).val().length > 0 ) {
-			$.user_log[ 'action' ] = $( '#action' ).val(); 
-		}
-	}
+	$.user_log[ 'data' ]   = '';
 	
 	if ( typeof( action ) != 'undefined' && action != null && action.length > 0 ) {
 		$.user_log[ 'action' ] = action;
 	}
-	
+
 	if ( typeof( data ) != 'undefined' && data != null && data.length > 0 ) {
 		$.user_log[ 'data' ] = data;
 	}
 	
-	$.user_log[ 'key' ] = $.user_data[ 'key' ];
+	$.user_log[ 'key' ] = $.register_user_data[ 'key' ];
 	
 	$.ajax({
 		type : 'POST',
@@ -394,10 +370,20 @@ $.register_log_action = function ( action, data ) {
 		data : ({
 			log: $.user_log
 		}),
-		success : function(data) {
-			$( '#progress-log' ).hide();
-			enableForm();
-		}
-	});
+		success : function( data ) {
+            if ( typeof callbackSuccess == 'function' ) {
+                callbackSuccess( data );
+            }
+		},
+        error: function( jqXHR, textStatus, errorThrown ) {
+            if ( typeof callbackError == 'function' ) {
+                var errorObject = {};
+                errorObject[ 'jqXHR' ]       = jqXHR;
+                errorObject[ 'textStatus' ]  = textStatus;
+                errorObject[ 'errorThrown' ] = errorThrown;
+                callbackError( errorObject );
+            }
+        }
+	}); // end log_user_action.php ajax call
 	
-};
+}; // end register_log_action()
