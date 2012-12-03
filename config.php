44652bd0609e159f7e0cda64960e6b540d90042b
Valentin Klein <v.klein@iconsultants.eu>
2012-12-03 14:10:42 +0100

    fix bugs

--- config.php
+++ config.php
@@ -2,18 +2,21 @@
 /**
  * Setup your app-model access credentials here
  */
-$aa_activated	= true;
-$aa_app_id 		= 153;
-$aa_app_secret 	= "770a51440ac4e9c2dbe7345a791fbfc4";
+$aa_activated = true;
+$aa_app_id = 153;
+$aa_app_secret = "770a51440ac4e9c2dbe7345a791fbfc4";
 
 /**
  * Setup your database access data
  */
-$db_activated 	= true;
-$db_host 		= "localhost";
-$db_name 		= "apps_aa_template";
-$db_user 		= "aa_template";
-$db_pass 		= "template";
+$db_activated = true;
+$db_host = "localhost";
+$db_name = "apps_aa_template";
+$db_user = "aa_template";
+$db_pass = "template";
 
+
+$fb_app_id = "161596157315481";
+$fb_app_secret = "cb11ea19d870420d6df37795cec44606";
 
 ?>
