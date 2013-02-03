27a54f389f3e8867fc983cd2a17835639ccfa05c
Sebastian Buckpesch <s.buckpesch@iconsultants.eu>
2013-02-03 02:03:15 +0100

    use jquery sync

--- index.php
+++ index.php
@@ -248,7 +248,7 @@ if (isset($aaForJs['inst']['aa_app_secret'])) {
 
 <button id="admin_panel_new">New Admin Panel Button</button>
 
-<script src="components/jquery/jquery.js"></script>
+<script src="js/components/jquery/jquery-1.7.1.min.js"></script>
 <!-- data-main attribute tells require.js to load scripts/main.js after require.js loads. -->
 <script data-main="js/main" src="js/require.js"></script>
 
